b7527d9dc1c5d7519d624ec715162d410282bed5
pushpavathi-wq <valasapushpa13@gmail.com>
2026-07-08 15:25:47 +0530

    sg

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -1,36 +1,15 @@
-resource "aws_ssm_parameter" "mysql-sg" {
-  name  = "/${var.project}/${var.environment}/mysql_sg_id"
-  type  = "String"
-  value = module.mysql-sg.sg_id
+variable "project_name" {
+    default = "expense"
 }
 
-resource "aws_ssm_parameter" "alb-ingress-sg" {
-  name  = "/${var.project}/${var.environment}/alb_ingress_sg_id"
-  type  = "String"
-  value = module.alb-ingress-sg.sg_id
+variable "environment" {
+    default = "dev"
 }
 
-
-resource "aws_ssm_parameter" "bastion_sg" {
-  name  = "/${var.project}/${var.environment}/bastion_sg_id"
-  type  = "String"
-  value = module.bastion_sg.sg_id
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
 }
-
-resource "aws_ssm_parameter" "eks-control-plane" {
-  name  = "/${var.project}/${var.environment}/eks_control_plane_sg_id"
-  type  = "String"
-  value = module.eks-control-plane-sg.sg_id
-}
-
-resource "aws_ssm_parameter" "node-sg" {
-  name  = "/${var.project}/${var.environment}/node_sg_id"
-  type  = "String"
-  value = module.node-sg.sg_id
-}
-
-
-
-
-
-
